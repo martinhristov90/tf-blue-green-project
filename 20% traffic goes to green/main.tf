a62b0b8564054d49e030c9888a6c2834a8aa6d74
nikcbg <43785382+nikcbg@users.noreply.github.com>
2019-03-12 13:14:38 +0200

    Update main.tf

--- 20% traffic goes to green/main.tf
+++ 20% traffic goes to green/main.tf
@@ -41,8 +41,8 @@ resource "aws_autoscaling_group" "example_green" {
   load_balancers    = ["${aws_elb.example.name}"]
   health_check_type = "ELB"
 
-  min_size = 2
-  max_size = 10
+  min_size = 1
+  max_size = 5
 
   tag {
     key                 = "Name"
@@ -58,8 +58,8 @@ resource "aws_autoscaling_group" "example_blue" {
   load_balancers    = ["${aws_elb.example.name}"]
   health_check_type = "ELB"
 
-  min_size = 8
-  max_size = 10
+  min_size = 4
+  max_size = 5
 
   tag {
     key                 = "Name"
